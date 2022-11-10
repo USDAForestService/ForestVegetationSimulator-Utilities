e38edee3d4ded63523851c614dd38b4b050feb74
markcastle1415 <mark.castle@usda.gov>
2022-11-10 12:52:46 -0700

    vegClass install update
    Added new install and updated vegClass_Example.R and FVSOut.db.
    
    Removed install.R and old version of vegClass documentation.

--- packages/vegClass/vegClassInstall/vegClass_Example.R
+++ packages/vegClass/vegClassInstall/vegClass_Example.R
@@ -1,22 +1,27 @@
-#############################################################################
+################################################################################
 #vegClass_Example.R
 #
 #This script provides an example of how to call the main function from the
 #vegClass package.
-#############################################################################
+################################################################################
+
+#How to run all lines of code in script (.R file):
+
+#RStudio: press Crtl + A key and then press Ctrl + Enter. Alternatively, after
+#         selecting all lines of code with Ctrl + A, you can press the Run
+#         button.
+
+#RGui: press Crtl + A key and then press Ctrl + R.
 
 #Attach vegClass package
 library(vegClass)
 
-#Example call to main function
+#Example call to main function in vegClass package
 main(input = "C:/Veg_Classification/vegClassInstall/FVSOut.db",
      output = "C:/Veg_Classification/vegClassInstall/FVSOut.csv",
-     overwriteOut = F,
-     groupTag = "eru=",
-     runTitles = c("MCD Run", "MEW Run"),
-     allRuns = F)
-
-#You can call function main as many times as you want in an R script or R
-#project. You can simply copy and paste the code above and change the arguments
-#as needed.
+     removeTag = "eru=",
+     runTitles = c("MCD Run", "Mew Run"),
+     addCompute = T,
+     addPotFire = T,
+     startYear = 2022)
 
